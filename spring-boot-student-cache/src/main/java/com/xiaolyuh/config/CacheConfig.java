71048b83670cd1696f2412ffb3cf6c88bafab9af
xiaodonghong <a250604@sina.com>
2019-07-25 17:08:52 +0800

    使用json存储

--- spring-boot-student-cache/src/main/java/com/xiaolyuh/config/CacheConfig.java
+++ spring-boot-student-cache/src/main/java/com/xiaolyuh/config/CacheConfig.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 /**
  * @ClassName CacheConfig
@@ -16,9 +19,12 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 @Configuration
 public class CacheConfig {
 
-//    @Bean
-//    public CacheManager cacheManager(RedisConnectionFactory factory, ObjectMapper objectMapper){
-//        return RedisCacheManager.
-//    }
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory, ObjectMapper objectMapper) {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer( Object.class );
+        jackson2JsonRedisSerializer.setObjectMapper( objectMapper );
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith( RedisSerializationContext.SerializationPair.fromSerializer( jackson2JsonRedisSerializer ) );
+        return RedisCacheManager.builder( factory ).cacheDefaults( redisCacheConfiguration ).build();
+    }
 
 }
