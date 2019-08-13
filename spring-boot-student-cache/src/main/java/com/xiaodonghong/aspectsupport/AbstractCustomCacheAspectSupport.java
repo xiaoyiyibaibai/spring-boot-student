b5fa3f62255e3ab3f0fc8bfcc70ded64c436b8ec
xiaodonghong <a250604@sina.com>
2019-08-13 18:09:25 +0800

    能走自定义了最简单的方式，未实现取数据和存数据。

--- spring-boot-student-cache/src/main/java/com/xiaodonghong/aspectsupport/AbstractCustomCacheAspectSupport.java
+++ spring-boot-student-cache/src/main/java/com/xiaodonghong/aspectsupport/AbstractCustomCacheAspectSupport.java
@@ -54,6 +54,15 @@ public abstract class AbstractCustomCacheAspectSupport extends CacheAspectSuppor
         this.cacheResolver = new SingletonSupplier<>(cacheResolver,
                 () -> CustomSimpleCacheResolver.of( SupplierUtils.resolve(cacheManager)));
     }
+    @Override
+    public void setKeyGenerator(KeyGenerator keyGenerator) {
+        this.keyGenerator = SingletonSupplier.of(keyGenerator);
+    }
+
+    @Override
+    public KeyGenerator getKeyGenerator() {
+        return this.keyGenerator.obtain();
+    }
 
     @Nullable
     @Override
@@ -107,8 +116,203 @@ public abstract class AbstractCustomCacheAspectSupport extends CacheAspectSuppor
 
     @Nullable
     private Object execute(final CacheOperationInvoker invoker, Method method, CacheOperationContexts contexts) {
+        if (contexts.isSynchronized()) {
+            CacheOperationContext context = contexts.get(CacheableOperation.class).iterator().next();
+            if (isConditionPassing(context, CacheOperationExpressionEvaluator.NO_RESULT)) {
+                Object key = generateKey(context, CacheOperationExpressionEvaluator.NO_RESULT);
+                Cache cache = context.getCaches().iterator().next();
+                try {
+                    return wrapCacheValue(method, cache.get(key, () -> unwrapReturnValue(invokeOperation(invoker))));
+                }
+                catch (Cache.ValueRetrievalException ex) {
+                    // The invoker wraps any Throwable in a ThrowableWrapper instance so we
+                    // can just make sure that one bubbles up the stack.
+                    throw (CacheOperationInvoker.ThrowableWrapper) ex.getCause();
+                }
+            }
+            else {
+                // No caching required, only call the underlying method
+                return invokeOperation(invoker);
+            }
+        }
+        // Process any early evictions
+        processCacheEvicts(contexts.get(CacheEvictOperation.class), true,
+                CacheOperationExpressionEvaluator.NO_RESULT);
+
+        // Check if we have a cached item matching the conditions
+        Cache.ValueWrapper cacheHit = findCachedItem(contexts.get(CacheableOperation.class));
+
+        // Collect puts from any @Cacheable miss, if no cached item is found
+        List<CachePutRequest> cachePutRequests = new LinkedList<>();
+        if (cacheHit == null) {
+            collectPutRequests(contexts.get(CacheableOperation.class),
+                    CacheOperationExpressionEvaluator.NO_RESULT, cachePutRequests);
+        }
+
+        Object cacheValue;
+        Object returnValue;
+
+        if (cacheHit != null && !hasCachePut(contexts)) {
+            // If there are no put requests, just use the cache hit
+            cacheValue = cacheHit.get();
+            returnValue = wrapCacheValue(method, cacheValue);
+        }
+        else {
+            // Invoke the method if we don't have a cache hit
+            returnValue = invokeOperation(invoker);
+            cacheValue = unwrapReturnValue(returnValue);
+        }
+
+        // Collect any explicit @CachePuts
+        collectPutRequests(contexts.get(CachePutOperation.class), cacheValue, cachePutRequests);
+
+        // Process any collected put requests, either from @CachePut or a @Cacheable miss
+        for (CachePutRequest cachePutRequest : cachePutRequests) {
+            cachePutRequest.apply(cacheValue);
+        }
+
+        // Process any late evictions
+        processCacheEvicts(contexts.get(CacheEvictOperation.class), false, cacheValue);
+
+        return returnValue;
+    }
+
+    private boolean hasCachePut(CacheOperationContexts contexts) {
+        // Evaluate the conditions *without* the result object because we don't have it yet...
+        Collection<CacheOperationContext> cachePutContexts = contexts.get(CachePutOperation.class);
+        Collection<CacheOperationContext> excluded = new ArrayList<>();
+        for (CacheOperationContext context : cachePutContexts) {
+            try {
+                if (!context.isConditionPassing(CacheOperationExpressionEvaluator.RESULT_UNAVAILABLE)) {
+                    excluded.add(context);
+                }
+            }
+            catch (VariableNotAvailableException ex) {
+                // Ignoring failure due to missing result, consider the cache put has to proceed
+            }
+        }
+        // Check if all puts have been excluded by condition
+        return (cachePutContexts.size() != excluded.size());
+    }
+
+    // TODO 查找缓存中是否存在
+    @Nullable
+    private Cache.ValueWrapper findCachedItem(Collection<CacheOperationContext> contexts) {
+        Object result = CacheOperationExpressionEvaluator.NO_RESULT;
+        for (CacheOperationContext context : contexts) {
+            if (isConditionPassing(context, result)) {
+                Object key = generateKey(context, result);
+                Cache.ValueWrapper cached = findInCaches(context, key);
+                if (cached != null) {
+                    return cached;
+                }
+                else {
+                    if (logger.isTraceEnabled()) {
+                        logger.trace("No cache entry for key '" + key + "' in cache(s) " + context.getCacheNames());
+                    }
+                }
+            }
+        }
         return null;
     }
+
+
+    private void collectPutRequests(Collection<CacheOperationContext> contexts,
+                                    @Nullable Object result, Collection<CachePutRequest> putRequests) {
+
+        for (CacheOperationContext context : contexts) {
+            if (isConditionPassing(context, result)) {
+                Object key = generateKey(context, result);
+                putRequests.add(new CachePutRequest(context, key));
+            }
+        }
+    }
+
+    @Nullable
+    private Cache.ValueWrapper findInCaches(CacheOperationContext context, Object key) {
+        for (Cache cache : context.getCaches()) {
+            Cache.ValueWrapper wrapper = doGet(cache, key);
+            if (wrapper != null) {
+                if (logger.isTraceEnabled()) {
+                    logger.trace("Cache entry for key '" + key + "' found in cache '" + cache.getName() + "'");
+                }
+                return wrapper;
+            }
+        }
+        return null;
+    }
+    // TODO  根据Collection<CacheOperationContext>执行删除操作
+    private void processCacheEvicts(
+            Collection<CacheOperationContext> contexts, boolean beforeInvocation, @Nullable Object result) {
+
+        for (CacheOperationContext context : contexts) {
+            CacheEvictOperation operation = (CacheEvictOperation) context.metadata.operation;
+            if (beforeInvocation == operation.isBeforeInvocation() && isConditionPassing(context, result)) {
+                performCacheEvict(context, operation, result);
+            }
+        }
+    }
+
+    private void performCacheEvict(
+            CacheOperationContext context, CacheEvictOperation operation, @Nullable Object result) {
+
+        Object key = null;
+        for (Cache cache : context.getCaches()) {
+            if (operation.isCacheWide()) {
+                logInvalidating(context, operation, null);
+                doClear(cache);
+            }
+            else {
+                if (key == null) {
+                    key = generateKey(context, result);
+                }
+                logInvalidating(context, operation, key);
+                doEvict(cache, key);
+            }
+        }
+    }
+
+    private void logInvalidating(CacheOperationContext context, CacheEvictOperation operation, @Nullable Object key) {
+        if (logger.isTraceEnabled()) {
+            logger.trace("Invalidating " + (key != null ? "cache key [" + key + "]" : "entire cache") +
+                    " for operation " + operation + " on method " + context.metadata.method);
+        }
+    }
+    private boolean isConditionPassing(CacheOperationContext context, @Nullable Object result) {
+        boolean passing = context.isConditionPassing(result);
+        if (!passing && logger.isTraceEnabled()) {
+            logger.trace("Cache condition failed on method " + context.metadata.method +
+                    " for operation " + context.metadata.operation);
+        }
+        return passing;
+    }
+
+    private Object generateKey(CacheOperationContext context, @Nullable Object result) {
+        Object key = context.generateKey(result);
+        if (key == null) {
+            throw new IllegalArgumentException("Null key returned for cache operation (maybe you are " +
+                    "using named params on classes without debug info?) " + context.metadata.operation);
+        }
+        if (logger.isTraceEnabled()) {
+            logger.trace("Computed cache key '" + key + "' for operation " + context.metadata.operation);
+        }
+        return key;
+    }
+
+    @Nullable
+    private Object wrapCacheValue(Method method, @Nullable Object cacheValue) {
+        if (method.getReturnType() == Optional.class &&
+                (cacheValue == null || cacheValue.getClass() != Optional.class)) {
+            return Optional.ofNullable(cacheValue);
+        }
+        return cacheValue;
+    }
+
+    @Nullable
+    private Object unwrapReturnValue(Object returnValue) {
+        return ObjectUtils.unwrapOptional(returnValue);
+    }
+
 
     @Override
     public void afterSingletonsInstantiated() {
